2b0203292c31701e0a3a8dd98b1bc0085b79045b
codaubianca <codau_bianca@yahoo.com>
2023-10-22 20:06:43 +0200

    stage 4 response with body

--- app/main.py
+++ app/main.py
@@ -9,11 +9,12 @@ def parse_request(data) -> {str, str}:
 def handle_request(data) -> str:
     method, path = parse_request(data)
     if method == "GET":
-        if path.startswith("/echo"):
+        if path == "/":
+            response = b"HTTP/1.1 200 OK\r\n\r\n"
+        elif path.startswith("/echo"):
             random_text = path.split("/")[1]
             text_len = len(random_text)
-            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {text_len}\r\n\r\n{random_text}".encode("utf-8")
-
+            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {text_len}\r\n\r\n{random_text}\r\n".encode("utf-8")
         else:
             response = b"HTTP/1.1 404 Not Found\r\n\r\n"
 
