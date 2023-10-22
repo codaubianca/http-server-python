ee9238849066380ce5efab667c9c2a829c80cbb6
codaubianca <codau_bianca@yahoo.com>
2023-10-22 19:33:00 +0200

    first server response 200 ok removed while true

--- app/main.py
+++ app/main.py
@@ -7,13 +7,12 @@ def main():
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     conn, addr = server_socket.accept() # wait for client
-    while True:
-        data = conn.recv(1024)
-        # if not data:
-        #     break
-        response = b"HTTP/1.1 200 OK\r\n\r\n"
-        conn.sendall(response)
-
+    data = conn.recv(1024)
+    # if not data:
+    #     break
+    response = b"HTTP/1.1 200 OK\r\n\r\n"
+    conn.send(response)
+    conn.close()
 
 
 if __name__ == "__main__":
