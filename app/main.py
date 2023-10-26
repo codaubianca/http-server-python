2a77d80df076ced088eb1f03a1b0c39da3c37f84
codaubianca <codau_bianca@yahoo.com>
2023-10-26 11:51:13 +0200

    stage 5

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 import socket
 def parse_request(data) -> {str, str}:
     data = data.decode("utf-8")
-    http_method = data.split("/r/n")[0]
+    data_lines = data.split("/r/n")
+    http_method = data_lines[0]
+    host = data_lines[2]
+    user_agent = data_lines[3]
     method = http_method.split()[0]
     path = http_method.split()[1]
-    return method, path
+    return method, path, host, user_agent
     
 def handle_request(data) -> str:
-    method, path = parse_request(data)
+    method, path, host, user_agent = parse_request(data)
     if method == "GET":
         if path == "/":
             response = b"HTTP/1.1 200 OK\r\n\r\n"
@@ -15,6 +18,9 @@ def handle_request(data) -> str:
             random_text = path[6:]
             text_len = len(random_text)
             response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {text_len}\r\n\r\n{random_text}".encode("utf-8")
+        elif path.startswith("/user-agent"):
+            text_len = len(user_agent)
+            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {text_len}\r\n\r\n{user_agent}".encode("utf-8")
         else:
             response = b"HTTP/1.1 404 Not Found\r\n\r\n"
 
